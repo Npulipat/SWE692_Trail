5dcfdec1666a51753ab3a54763acf25148df3579
Nagendra0822 <nagendrababupulipati@gmail.com>
2024-02-28 14:28:40 -0500

    Added a function to initialise GPIO pins

--- Traffic_Signal.c
+++ Traffic_Signal.c
@@ -1,1 +1,54 @@
+// Function to export a GPIO pin
+int32_t export_gpio(int32_t pin) {
+    //todo
+}
 
+// Function to set the direction of a GPIO pin
+int32_t set_gpio_direction(int32_t pin, char *direction) {
+    //todo
+}
+
+
+
+// Function to initialize GPIO pins
+int32_t init_gpio(int32_t red_pin_A, int32_t yellow_pin_A, int32_t green_pin_A, int32_t red_pin_B, int32_t yellow_pin_B, int32_t green_pin_B) {
+    int32_t status = 0;
+
+    // Export and set directions for GPIO pins
+    status |= export_gpio(red_pin_A);
+    status |= export_gpio(yellow_pin_A);
+    status |= export_gpio(green_pin_A);
+    status |= export_gpio(red_pin_B);
+    status |= export_gpio(yellow_pin_B);
+    status |= export_gpio(green_pin_B);
+
+    status |= set_gpio_direction(red_pin_A, "out");
+    status |= set_gpio_direction(yellow_pin_A, "out");
+    status |= set_gpio_direction(green_pin_A, "out");
+    status |= set_gpio_direction(red_pin_B, "out");
+    status |= set_gpio_direction(yellow_pin_B, "out");
+    status |= set_gpio_direction(green_pin_B, "out");
+
+    // Return status indicating success or failure
+    return status;
+}
+
+int32_t main() {
+    int32_t red_pin_A, yellow_pin_A, green_pin_A, red_pin_B, yellow_pin_B, green_pin_B;
+    int32_t sleep_interval;
+
+    // Prompt user for GPIO pins and sleep interval
+    (void)printf("Enter GPIO pins for Traffic Light Set A (Red Yellow Green): ");
+    scanf("%d %d %d", &red_pin_A, &yellow_pin_A, &green_pin_A);
+    (void)printf("Enter GPIO pins for Traffic Light Set B (Red Yellow Green): ");
+    scanf("%d %d %d", &red_pin_B, &yellow_pin_B, &green_pin_B);
+    (void)printf("Enter sleep interval (in seconds): ");
+    scanf("%d", &sleep_interval);
+
+    // Initialize GPIO pins
+    if (init_gpio(red_pin_A, yellow_pin_A, green_pin_A, red_pin_B, yellow_pin_B, green_pin_B) != 0) {
+        (void)printf("Failed to initialize GPIO pins. Exiting.\n");
+        return 1;
+    }
+    return 0;
+}
